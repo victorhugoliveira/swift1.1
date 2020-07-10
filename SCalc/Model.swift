3e26222ba169151cad1c577d4ff2b21d8f0b34c3
Victor Hugo Oliveira <62809626+oliveiravh@users.noreply.github.com>
2020-07-09 22:07:14 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -16,50 +16,7 @@ open class Model {
     var state = "firstNumberState"
     var negation = false
     
-    
-    func clearLabel(){
-        // Basically resets everything
-        currentNumberStr = "0"
-        operation = ""
-        firstNumber = 0.0
-        secondNumber = 0.0
-    }
-    
-    func appendToNumberStr(numStr : String){
-        if(currentNumberStr.count<11){
-            if ((numStr != "0") || (currentNumberStr != "0")){
-                if((currentNumberStr == "0")&&(numStr != ".")){
-                    currentNumberStr = numStr
-                }else{
-                    currentNumberStr.append(numStr)
-                }
-            }
-        }
-    }    
-    
-    func executeOperation() -> (num: Double, status: String) {
-        var result = 0.0
-        var statusStr = "="
-        switch operation{
-        case "+":
-            result = firstNumber + secondNumber
-        case "-":
-            result = firstNumber - secondNumber
-        case "x":
-            result = firstNumber * secondNumber
-        case "/":
-            if(secondNumber==0.0){
-                statusStr = "err /0"
-                result = 0
-            }else{
-                result = firstNumber / secondNumber
-            }
-        default:
-            statusStr = "no op"
-        }
-        return (result,statusStr)
-    }
-    
+
     
     func treatEvent(event : String) -> (num: String, status: String) {
         var statusStr = ""
