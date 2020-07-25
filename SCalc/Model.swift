57d8e65bf60a6b80a0111a023e38a6b26a399aa1
Victor Hugo Oliveira <62809626+oliveiravh@users.noreply.github.com>
2020-07-25 10:40:32 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -16,100 +16,17 @@ open class Model {
     var state = "firstNumberState"
     var negation = false
     
-    func treatEvent(event : String) -> Result {
-        var statusStr = ""
-        switch state{
-        case "firstNumberState":
-            switch event{
-            case ".":
-                if (!currentNumberStr.contains(".")){
-                    appendToNumberStr(numStr: event)
-                    statusStr = "temp"
-                }
-            case "0","1","2","3","4","5","6","7","8","9":
-                var extra = ""
-                if(negation){
-                    extra = "-"
-                    negation = false
-                }
-                appendToNumberStr(numStr: extra+event)
-                if (event=="0"){
-                    if(currentNumberStr.contains(".")){
-                        statusStr = "temp"
-                    }
-                }
-            case "+","-","x","/":
-                if (currentNumberStr != "0"){
-                    operation = event
-                    firstNumber = Double(currentNumberStr)!
-                    let firstNumberStr = currentNumberStr
-                    state = "secondNumberState"
-                    currentNumberStr = "0"
-                    return Result(numberStr: firstNumberStr, statusStr: event)
+      func appendToNumberStr(numStr : String){
+        if(currentNumberStr.count<11){
+            if ((numStr != "0") || (currentNumberStr != "0")){
+                if((currentNumberStr == "0")&&(numStr != ".")){
+                    currentNumberStr = numStr
                 }else{
-                    if((event == "-")&&(firstNumber == 0.0)){
-                        negation = true
-                    }
+                    currentNumberStr.append(numStr)
                 }
-            case "Invert":
-                firstNumber = Double(currentNumberStr)!
-                firstNumber = (-1) * firstNumber
-                currentNumberStr = "0"
-                appendToNumberStr(numStr: String(firstNumber))
-            case "Clear":
-                clearLabel()
-            default:
-                return Result(numberStr: currentNumberStr, statusStr: "")
             }
-        case "secondNumberState" :
-            secondNumber = Double(currentNumberStr)!
-            switch event{
-            case ".":
-                if (!currentNumberStr.contains(".")){
-                    appendToNumberStr(numStr: event)
-                    statusStr = "temp"
-                }
-            case "0","1","2","3","4","5","6","7","8","9":
-                appendToNumberStr(numStr: event)
-                if (event=="0"){
-                    if(currentNumberStr.contains(".")){
-                        statusStr = "temp"
-                    }
-                }
-            case "+","-","x","/":
-                if (secondNumber != 0.0) {
-                    let result = executeOperation()
-                    firstNumber = result.num
-                    statusStr = result.status
-                    secondNumber = 0.0
-                    currentNumberStr = "0"
-                }
-                let firstNumberStr = firstNumber.toString(decimal: 11)
-                operation = event
-                return Result(numberStr: firstNumberStr, statusStr: operation)
-            case "Invert":
-                secondNumber = (-1) * secondNumber
-                currentNumberStr = "0"
-                appendToNumberStr(numStr: String(secondNumber))
-            case "=":
-                let result = executeOperation()
-                firstNumber = result.num
-                statusStr = result.status
-                secondNumber = 0
-                currentNumberStr = firstNumber.toString(decimal: 11)
-                state = "firstNumberState"
-                return Result(numberStr: currentNumberStr, statusStr: statusStr)
-            case "Clear":
-                state = "firstNumberState"
-                clearLabel()
-            default:
-                return Result(numberStr: currentNumberStr, statusStr: statusStr)
-            }
-        default:
-            print("state: default")
         }
-        return Result(numberStr: currentNumberStr, statusStr: statusStr)
-    }
+     }   
     
     
 }
