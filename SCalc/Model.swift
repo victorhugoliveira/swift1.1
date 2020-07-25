1fb845306328590c22f75b33b03e9ff2d1147d3b
Victor Hugo Oliveira <62809626+oliveiravh@users.noreply.github.com>
2020-07-25 10:52:58 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -8,22 +8,24 @@
 
 import Foundation
 
-extension Double {    
-    func toString(decimal: Int = 9) -> String {
-        let value = decimal < 0 ? 0 : decimal
-        var string = String(format: "%.\(value)f", self)
-        
-        while string.last == "0" || string.last == "." {
-            if string.last == "." { string = String(string.dropLast()); break}
-            string = String(string.dropLast())
-        }
-        return string
-    }
-}
-
-
 
 open class Model {
-
+    var currentNumberStr = "0"
+    var firstNumber = 0.0
+    var secondNumber = 0.0
+    var operation = ""
+    var state = "firstNumberState"
+    var negation = false
+    
+    
+    func clearLabel(){
+        // Basically resets everything
+        currentNumberStr = "0"
+        operation = ""
+        firstNumber = 0.0
+        secondNumber = 0.0
+    }
+    
+    
     
 }
