4d64beea688e7d3d1fc732b07af98d421ba60308
Victor Hugo Oliveira <62809626+oliveiravh@users.noreply.github.com>
2020-07-25 10:31:39 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -8,19 +8,6 @@
 
 import Foundation
 
-extension Double {    
-    func toString(decimal: Int = 9) -> String {
-        let value = decimal < 0 ? 0 : decimal
-        var string = String(format: "%.\(value)f", self)
-        
-        while string.last == "0" || string.last == "." {
-            if string.last == "." { string = String(string.dropLast()); break}
-            string = String(string.dropLast())
-        }
-        return string
-    }
-}
-
 open class Model {
     var currentNumberStr = "0"
     var firstNumber = 0.0
@@ -28,51 +15,6 @@ open class Model {
     var operation = ""
     var state = "firstNumberState"
     var negation = false
-    
-    
-    func clearLabel(){
-        // Basically resets everything
-        currentNumberStr = "0"
-        operation = ""
-        firstNumber = 0.0
-        secondNumber = 0.0
-    }
-    
-    func appendToNumberStr(numStr : String){
-        if(currentNumberStr.count<11){
-            if ((numStr != "0") || (currentNumberStr != "0")){
-                if((currentNumberStr == "0")&&(numStr != ".")){
-                    currentNumberStr = numStr
-                }else{
-                    currentNumberStr.append(numStr)
-                }
-            }
-        }
-    }    
-    
-    func executeOperation() -> (num: Double, status: String) {
-        var result = 0.0
-        var statusStr = "="
-        switch operation{
-        case "+":
-            result = firstNumber + secondNumber
-        case "-":
-            result = firstNumber - secondNumber
-        case "x":
-            result = firstNumber * secondNumber
-        case "/":
-            if(secondNumber==0.0){
-                statusStr = "err* /0"
-                result = 0
-            }else{
-                result = firstNumber / secondNumber
-            }
-        default:
-            statusStr = "no op"
-        }
-        return (result,statusStr)
-    }
-    
     
     func treatEvent(event : String) -> Result {
         var statusStr = ""
