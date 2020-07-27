e307f102afdde9e2cc89a9205a26e51a2241572a
Victor Hugo Oliveira <62809626+oliveiravh@users.noreply.github.com>
2020-07-26 21:19:00 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -9,14 +9,7 @@
 import Foundation
 
 open class Model {
-    var currentNumberStr = "0"
-    var firstNumber = 0.0
-    var secondNumber = 0.0
-    var operation = ""
-    var state = "firstNumberState"
-    var negation = false
-    
-    func executeOperation() -> (num: Double, status: String) {
+   func executeOperation() -> (num: Double, status: String) {
         var result = 0.0
         var statusStr = "="
         switch operation{
@@ -37,5 +30,93 @@ open class Model {
             statusStr = "no op"
         }
         return (result,statusStr)
+    }
+    
+    
+    func treatEvent(event : String) -> (num: String, status: String) {
+        var statusStr = ""
+        switch state{
+        case "firstNumberState":
+            switch event{
+            case ".":
+                if (!currentNumberStr.contains(".")){
+                    appendToNumberStr(numStr: event)
+                }
+            case "0","1","2","3","4","5","6","7","8","9":
+                var extra = ""
+                if(negation){
+                    extra = "-"
+                    negation = false
+                }
+                appendToNumberStr(numStr: extra+event)
+            case "+","-","x","/":
+                if (currentNumberStr != "0"){
+                    operation = event
+                    firstNumber = Double(currentNumberStr)!
+                    state = "secondNumberState"
+                    currentNumberStr = "0"
+                    return (String(firstNumber),event)
+                }else{
+                    if((event == "-")&&(firstNumber == 0.0)){
+                        negation = true
+                    }
+                }
+            case "Invert":
+                firstNumber = Double(currentNumberStr)!
+                firstNumber = (-1) * firstNumber
+                currentNumberStr = "0"
+                appendToNumberStr(numStr: String(firstNumber))
+            case "Clear":
+                clearLabel()
+            default:
+                return (currentNumberStr,"")
+            }
+        case "secondNumberState" :
+            secondNumber = Double(currentNumberStr)!
+            switch event{
+            case ".":
+                if (!currentNumberStr.contains(".")){
+                    appendToNumberStr(numStr: event)
+                }
+            case "0","1","2","3","4","5","6","7","8","9":
+                appendToNumberStr(numStr: event)
+            case "+","-","x","/":
+                if (secondNumber != 0.0) {
+                    let result = executeOperation()
+                    firstNumber = result.num
+                    statusStr = result.status
+                    secondNumber = 0.0
+                    currentNumberStr = "0"
+                }
+                operation = event
+                return (String(firstNumber),operation)
+            case "Invert":
+                secondNumber = (-1) * secondNumber
+                currentNumberStr = "0"
+                appendToNumberStr(numStr: String(secondNumber))
+            case "=":
+                let result = executeOperation()
+                firstNumber = result.num
+                statusStr = result.status
+                secondNumber = 0
+                
+                currentNumberStr = String(firstNumber)
+                if(floor(firstNumber)==firstNumber){
+                    let index = currentNumberStr.firstIndex(of: ".") ?? currentNumberStr.endIndex
+                    let beginning = currentNumberStr[..<index]
+                    currentNumberStr = String(beginning)                    
+                }
+ 
+                state = "firstNumberState"
+                return (String(firstNumber),statusStr)
+            case "Clear":
+                state = "firstNumberState"
+                clearLabel()
+            default:
+                return (currentNumberStr,statusStr)
+            }        default:
+                print("state: default")
+        }
+        return (currentNumberStr,statusStr)
     }
 }
