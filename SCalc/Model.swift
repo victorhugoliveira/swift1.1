6325d2a6f17545f914a572a3e29e84c1db5d6af6
victorhugoliveira <oliveirvh@gmail.com>
2020-08-09 21:36:02 -0300

    Update Model.swift

--- SCalc/Model.swift
+++ SCalc/Model.swift
@@ -7,8 +7,16 @@
 //
 
 import Foundation
-
+// v2
 open class Model {
+      
+          var currentNumberStr = "0"
+    var firstNumber = 0.0
+    var secondNumber = 0.0
+    var operation = ""
+    var state = "firstNumberState"
+    var negation = false
+      
       func treatEvent(event : String) -> Result {
         var statusStr = ""
         switch state{
